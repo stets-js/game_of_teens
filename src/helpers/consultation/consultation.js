8d670e7809321e6b523e70ef4a22f6474e583584
Dmytro Stetsenko <97848708+stets-js@users.noreply.github.com>
2023-12-18 18:16:12 +0200

    Update consultation.js

--- src/helpers/consultation/consultation.js
+++ src/helpers/consultation/consultation.js
@@ -7,7 +7,20 @@ const postConsultationResult = (slotId, result, groupId, message) => {
         ? `/consultation_result/${slotId}/${result}/${groupId}/${message}`
         : `/consultation_result/${slotId}/${result}/${groupId}/no-text`
     )
-    .then((res) => res.data)
+    .then((res) => {
+      const responseData = {
+        ...res.data,
+        action: "attended",
+      };
+
+      axios.post(
+        "https://zohointegration.goit.global/GoITeens/booking/index.php",
+        JSON.stringify(responseData),
+        { headers: { "Content-Type": "application/json" }}
+      );
+
+      return res.data;
+    })
     .catch((error) => {
       throw error;
     });
