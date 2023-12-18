1a464620a590ce92f028416fe68e22a49d6dcb61
Dmytro Stetsenko <97848708+stets-js@users.noreply.github.com>
2023-12-18 21:51:16 +0200

    Update confirmation.js

--- src/helpers/confirmation/confirmation.js
+++ src/helpers/confirmation/confirmation.js
@@ -19,26 +19,56 @@ const getConfirmatorWeekData = (weekId, dayId, halfId) => {
 };
 
 const setConfirmation = (slot_id, status, message) => {
+  const url = window.location.href.split('/');
+  const confirmatorId = +url[url.length-1];
   return axios
     .post(
       message
-        ? `/set_confirmation/${slot_id}/${status}/${message}`
-        : `/set_confirmation/${slot_id}/${status}`
+        ? `/set_confirmation/${slot_id}/${status}/${message}/${confirmatorId}/`
+        : `/set_confirmation/${slot_id}/${status}/${confirmatorId}/`
     )
-    .then((res) => res.data)
+    .then((res) => {
+      const responseData = {
+        ...res.data,
+        action: "confirmed",
+      };
+
+      axios.post(
+        "https://zohointegration.goit.global/GoITeens/booking/index.php",
+        JSON.stringify(responseData),
+        { headers: { "Content-Type": "application/json" }}
+      );
+
+      return res.data;
+    })
     .catch((error) => {
       throw error;
     });
 };
 
 const setCancelConfirmation = (slot_id, status, message) => {
+  const url = window.location.href.split('/');
+  const confirmatorId = +url[url.length-1];
   return axios
     .post(
       message
-        ? `/set_cancel_confirmation/${slot_id}/${status}/${message}`
-        : `/set_cancel_confirmation/${slot_id}/${status}`
+        ? `/set_cancel_confirmation/${slot_id}/${status}/${message}/${confirmatorId}/`
+        : `/set_cancel_confirmation/${slot_id}/${status}/${confirmatorId}/`
     )
-    .then((res) => res.data)
+    .then((res) => {
+      const responseData = {
+        ...res.data,
+        action: "canceled",
+      };
+
+      axios.post(
+        "https://zohointegration.goit.global/GoITeens/booking/index.php",
+        JSON.stringify(responseData),
+        { headers: { "Content-Type": "application/json" }}
+      );
+
+      return res.data;
+    })
     .catch((error) => {
       throw error;
     });
