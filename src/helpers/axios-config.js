12ac9c1f116cf497fdcb83a4ea512ef852b5d029
Dmytro Stetsenko <97848708+stets-js@users.noreply.github.com>
2024-02-04 16:48:41 +0200

    Update axios-config.js

--- src/helpers/axios-config.js
+++ src/helpers/axios-config.js
@@ -3,31 +3,31 @@ axios.defaults.baseURL = "https://king-prawn-app-hnaei.ondigitalocean.app";
 axios.defaults.headers.common["Accept"] = "application/json";
 // axios.defaults.headers.common['Authorization'] = `Bearer ${localStorage.getItem('booking')}`;
 
-axios.interceptors.request.use(
-    config => {
-      const bookingToken = localStorage.getItem('booking');
-      if (bookingToken) {
-        config.headers['Authorization'] = `Bearer ${bookingToken}`;
-      }
-      return config;
-    },
-    error => {
-      return Promise.reject(error);
-    }
-  );
+// axios.interceptors.request.use(
+//     config => {
+//       const bookingToken = localStorage.getItem('booking');
+//       if (bookingToken) {
+//         config.headers['Authorization'] = `Bearer ${bookingToken}`;
+//       }
+//       return config;
+//     },
+//     error => {
+//       return Promise.reject(error);
+//     }
+//   );
   
-  axios.interceptors.response.use(
-    response => {
-      return response;
-    },
-    async error => {
-      if (error.response && error.response.status === 401) {
-        localStorage.removeItem('booking');
-        return Promise.reject(error);
-      }
+//   axios.interceptors.response.use(
+//     response => {
+//       return response;
+//     },
+//     async error => {
+//       if (error.response && error.response.status === 401) {
+//         localStorage.removeItem('booking');
+//         return Promise.reject(error);
+//       }
   
-      return Promise.reject(error);
-    }
-  );
+//       return Promise.reject(error);
+//     }
+//   );
 
 export default axios;
