263e976210b06c781b3f1cb1dd5f7f50ac01a650
Dmytro Stetsenko <97848708+stets-js@users.noreply.github.com>
2024-02-04 12:53:12 +0200

    Update axios-config.js

--- src/helpers/axios-config.js
+++ src/helpers/axios-config.js
@@ -2,4 +2,8 @@ import axios from "axios";
 axios.defaults.baseURL = "https://king-prawn-app-hnaei.ondigitalocean.app";
 axios.defaults.headers.common["Accept"] = "application/json";
 
+const bookingToken = localStorage.getItem('booking');
+if (bookingToken) {
+    axios.defaults.headers.common['Authorization'] = `Bearer ${bookingToken}`;
+}
 export default axios;
