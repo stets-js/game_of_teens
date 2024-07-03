3ee339fe1d5555350a4e9efbca7a7c24be986de2
Sheeseburger <kekymber@gmail.com>
2024-07-03 16:15:58 +0300

    forgot about axios-config

--- src/helpers/axios-config.js
+++ src/helpers/axios-config.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
-// axios.defaults.baseURL = "https://back-gameofteens.onrender.com";
-axios.defaults.baseURL = 'http://127.0.0.1:8080';
+axios.defaults.baseURL = 'https://back-gameofteens.onrender.com';
+// axios.defaults.baseURL = 'http://127.0.0.1:8080';
 axios.defaults.headers.common['Accept'] = 'application/json';
 
 axios.interceptors.request.use(
