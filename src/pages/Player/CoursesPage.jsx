958d82c1cc1ce6b497347cc9426e466faabfd142
ksesly <ks.you.04@gmail.com>
2024-07-04 16:34:56 +0300

    cources page

--- src/pages/Player/CoursesPage.jsx
+++ src/pages/Player/CoursesPage.jsx
@@ -3,6 +3,9 @@ import styles from './PlayerPage.module.scss';
 import PlayerHeader from '../../components/PlayerHeader/PlayerHeader';
 import {getAllMarathons} from '../../helpers/marathon/marathon';
 import MarathonCard from '../../components/Marathon/MarathonCard';
+
+
+
 export default function CoursesPage() {
   const [marathons, setMarathons] = useState([]);
   const fetchAllMarathons = async () => {
