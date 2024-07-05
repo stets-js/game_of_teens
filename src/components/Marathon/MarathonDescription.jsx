f609c1e76f9e1565d4748f636c6e169a5d158544
Sheeseburger <kekymber@gmail.com>
2024-07-05 15:01:27 +0300

    added tabulation to desc

--- src/components/Marathon/MarathonDescription.jsx
+++ src/components/Marathon/MarathonDescription.jsx
@@ -1,11 +1,19 @@
 import React from 'react';
-
+import styles from './MarathonCard.module.scss';
 export default function MarathonDescription({description}) {
   const lines = description.split('\n');
 
   return (
     <>
-      {lines.map((line, index) => (line === '' ? <br key={index} /> : <p key={index}>{line}</p>))}
+      {lines.map((line, index) =>
+        line === '' ? (
+          <br key={index} />
+        ) : (
+          <p className={styles.indented} key={index}>
+            {line}
+          </p>
+        )
+      )}
     </>
   );
 }
