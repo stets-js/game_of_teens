ec21386534e5a6074a4631c098c95222378243b4
Sheeseburger <kekymber@gmail.com>
2024-07-06 14:50:02 +0300

    hidded courses

--- src/components/PlayerHeader/PlayerHeader.jsx
+++ src/components/PlayerHeader/PlayerHeader.jsx
@@ -4,49 +4,46 @@ import styles from './PlayerHeader.module.scss';
 import logo from '../../img/goiteensLOGO.png';
 import logout from '../../img/logout.svg';
 
-
 export default function PlayerHeader() {
-	const userName = useSelector((state) => state.auth.user.name);
-	const userId = useSelector((state) => state.auth.user.id);
-	const dispatch = useDispatch();
+  const userName = useSelector(state => state.auth.user.name);
+  const userId = useSelector(state => state.auth.user.id);
+  const dispatch = useDispatch();
 
-
-	return (
-		<>
-			<div className={styles.list__wrapper}>
-				<div className={styles.list__item}>
-					<a href={`/player/${userId}`}>
-						<img src={logo} alt="logo" className={styles.logo} />{' '}
-					</a>
-				</div>
-				<div className={styles.list__item}>
-					<a href={`/player/${userId}`}>
-						<p>Профіль</p>
-					</a>
-				</div>
-				<div className={styles.list__item}>
+  return (
+    <>
+      <div className={styles.list__wrapper}>
+        <div className={styles.list__item}>
+          <a href={`/player/${userId}`}>
+            <img src={logo} alt="logo" className={styles.logo} />{' '}
+          </a>
+        </div>
+        <div className={styles.list__item}>
+          <a href={`/player/${userId}`}>
+            <p>Профіль</p>
+          </a>
+        </div>
+        {/* <div className={styles.list__item}>
 					<a href={`/player/${userId}/courses`}>
 						<p>Курси</p>
 					</a>
-				</div>
-				<div className={styles.list__account}>
-					<label htmlFor="avatar" className={styles.list__account__label}>
-						{userName}
-					</label>
-					<button
-						type="button"
-						className={styles.list__account__logout}
-						onClick={() => {
-							localStorage.removeItem('got');
-							dispatch({
-								type: 'LOGOUT',
-							});
-						}}
-					>
-						<img src={logout} alt="logout" />
-					</button>
-				</div>
-			</div>
-		</>
-	);
+				</div> */}
+        <div className={styles.list__account}>
+          <label htmlFor="avatar" className={styles.list__account__label}>
+            {userName}
+          </label>
+          <button
+            type="button"
+            className={styles.list__account__logout}
+            onClick={() => {
+              localStorage.removeItem('got');
+              dispatch({
+                type: 'LOGOUT'
+              });
+            }}>
+            <img src={logout} alt="logout" />
+          </button>
+        </div>
+      </div>
+    </>
+  );
 }
