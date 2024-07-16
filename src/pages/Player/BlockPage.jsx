91f546d6c399ccf5e63e3402b171c98fd74ac500
Sheeseburger <kekymber@gmail.com>
2024-07-16 15:09:52 +0300

    hidden edit and delete for not users

--- src/pages/Player/BlockPage.jsx
+++ src/pages/Player/BlockPage.jsx
@@ -1,46 +1,41 @@
-import classNames from "classnames";
-import React, { useEffect, useState } from "react";
-import { useConfirm } from "material-ui-confirm";
-import { useNavigate, useLocation, useParams } from "react-router-dom";
-import PlayerHeader from "../../components/PlayerHeader/PlayerHeader";
-import styles from "./PlayerPage.module.scss";
-import buttonStyles from "../../styles/Button.module.scss";
-import { useSelector } from "react-redux";
-import { getTeamAsMember } from "../../helpers/team/team";
+import classNames from 'classnames';
+import React, {useEffect, useState} from 'react';
+import {useConfirm} from 'material-ui-confirm';
+import {useNavigate, useLocation, useParams} from 'react-router-dom';
+import PlayerHeader from '../../components/PlayerHeader/PlayerHeader';
+import styles from './PlayerPage.module.scss';
+import buttonStyles from '../../styles/Button.module.scss';
+import {useSelector} from 'react-redux';
+import {getTeamAsMember} from '../../helpers/team/team';
 import {
   createProjectToBlock,
   getProjectByTeamId,
-  deleteBlockById,
-} from "../../helpers/marathon/marathon";
-import MDEditor from "@uiw/react-md-editor";
-import { success, error, defaults } from '@pnotify/core';
-import ChatComponent from "../../components/Chat/ChatComponent";
-import ProjectList from "../../components/Mentor/ProjectList";
-import UploadFiles from "../../components/Block/UploadFiles";
-import UploadedData from "../../components/Block/UploadedDate";
+  deleteBlockById
+} from '../../helpers/marathon/marathon';
+import MDEditor from '@uiw/react-md-editor';
+import {success, error, defaults} from '@pnotify/core';
+import ChatComponent from '../../components/Chat/ChatComponent';
+import ProjectList from '../../components/Mentor/ProjectList';
+import UploadFiles from '../../components/Block/UploadFiles';
+import UploadedData from '../../components/Block/UploadedDate';
 
 export default function BlockPage() {
   const navigate = useNavigate();
   const location = useLocation();
-  const { marathon } = location.state;
-  const { sprintId } = useParams() || null;
-  const { courseId } = useParams() || null;
-  console.log("courseId", courseId);
-  const userId = useSelector((state) => state.auth.user.id);
-  const userRole = useSelector((state) => state.auth.user.role);
-  const [block] = useState(
-    marathon.blocks.filter((bl) => bl._id === sprintId)[0]
-  );
+  const {marathon} = location.state;
+  const {sprintId} = useParams() || null;
+  const {courseId} = useParams() || null;
+  console.log('courseId', courseId);
+
+  const userId = useSelector(state => state.auth.user.id);
+  const userRole = useSelector(state => state.auth.user.role);
+  const [block] = useState(marathon.blocks.filter(bl => bl._id === sprintId)[0]);
   const [myProject, setMyProject] = useState(null);
   const [myTeam, setMyTeam] = useState(null);
-console.log("block", block)
+  console.log('block', block);
   const fetchMyProject = async () => {
     try {
-      const data = await getProjectByTeamId(
-        marathon._id,
-        block._id,
-        myTeam._id
-      );
+      const data = await getProjectByTeamId(marathon._id, block._id, myTeam._id);
       if (data) setMyProject(data);
     } catch (error) {
       console.error(error);
@@ -49,7 +44,7 @@ console.log("block", block)
 
   const fetchMyTeam = async () => {
     try {
-      const { data } = await getTeamAsMember(userId, marathon._id);
+      const {data} = await getTeamAsMember(userId, marathon._id);
       setMyTeam(data.data[0]);
     } catch (error) {
       console.error(error);
@@ -66,11 +61,11 @@ console.log("block", block)
 
   const postProject = async () => {
     if (myTeam.leader._id !== userId) {
-      return error({ text: "Лише лідер може це зробити", delay: 1000 });
+      return error({text: 'Лише лідер може це зробити', delay: 1000});
     }
     try {
-      const { data } = await createProjectToBlock(marathon._id, block._id, {
-        team: myTeam._id,
+      const {data} = await createProjectToBlock(marathon._id, block._id, {
+        team: myTeam._id
       });
       setMyProject(data);
     } catch (error) {
@@ -83,10 +78,10 @@ console.log("block", block)
       if (marathonId && blockId) {
         await deleteBlockById(marathonId, blockId, {
           name: block.name,
-          description: block.description,
-        })
-        success("Block successfully deleted!"); 
-        navigate(`/mentor/${userId}`)       
+          description: block.description
+        });
+        success('Block successfully deleted!');
+        navigate(`/mentor/${userId}`);
       }
     } catch (error) {
       console.error(error);
@@ -98,35 +93,40 @@ console.log("block", block)
       <PlayerHeader />
       <div className={styles.block__wrapper}>
         <div className={styles.block__header}>{block.name}</div>
-        <div className={styles.block__btnsWrapper}>
-          <button
-            className={styles.block__btn__edit}
-            type="button"
-            onClick={() =>
-              navigate(`/mentor/${userId}/courses/${courseId}/edit_block`, {
-                state: { marathon_state: marathon, title: block.name, description: block.description, blockId: block._id },
-              })
-            }
-          >
-            Edit block
-          </button>
-          <button
-            className={styles.block__btn__delete}
-            type="button"
-            onClick={() => { deleteBlock(marathon._id, block._id); }}
-          >
-            Delete block
-          </button>
-        </div>
+        {userRole !== 2 && (
+          <div className={styles.block__btnsWrapper}>
+            <button
+              className={styles.block__btn__edit}
+              type="button"
+              onClick={() =>
+                navigate(`/mentor/${userId}/courses/${courseId}/edit_block`, {
+                  state: {
+                    marathon_state: marathon,
+                    title: block.name,
+                    description: block.description,
+                    blockId: block._id
+                  }
+                })
+              }>
+              Edit block
+            </button>
+            <button
+              className={styles.block__btn__delete}
+              type="button"
+              onClick={() => {
+                deleteBlock(marathon._id, block._id);
+              }}>
+              Delete block
+            </button>
+          </div>
+        )}
         {!myTeam && (
           <p className={classNames(styles.confirmed, styles.checkedByMentor)}>
             ⚠️Треба створити команду для здачі⚠️ (додавати людей не обов'язково)
           </p>
         )}
         {myProject && myProject.confirm && (
-          <p className={classNames(styles.confirmed, styles.checkedByMentor)}>
-            Здано ✔️
-          </p>
+          <p className={classNames(styles.confirmed, styles.checkedByMentor)}>Здано ✔️</p>
         )}
         {myProject && myProject.checkedByMentor && (
           <p className={styles.checkedByMentor}>Перевірено ментором ✔️</p>
@@ -135,34 +135,26 @@ console.log("block", block)
           <MDEditor.Markdown
             mode="light"
             source={block.description}
-            style={{ whiteSpace: "pre-wrap" }}
+            style={{whiteSpace: 'pre-wrap'}}
           />
         </div>
         {userRole === 2 &&
           (myProject ? (
             <>
-              {myTeam &&
-                !myProject.confirm &&
-                myTeam?.leader?._id === userId && (
-                  <UploadFiles
-                    marathon={marathon}
-                    blockId={block._id}
-                    myProject={myProject}
-                    setMyProject={setMyProject}
-                  />
-                )}
-              <UploadedData
-                files={myProject.files}
-                links={myProject.links}
-              />
+              {myTeam && !myProject.confirm && myTeam?.leader?._id === userId && (
+                <UploadFiles
+                  marathon={marathon}
+                  blockId={block._id}
+                  myProject={myProject}
+                  setMyProject={setMyProject}
+                />
+              )}
+              <UploadedData files={myProject.files} links={myProject.links} />
             </>
           ) : (
             <>
               {myTeam && (
-                <button
-                  className={buttonStyles.button}
-                  onClick={postProject}
-                >
+                <button className={buttonStyles.button} onClick={postProject}>
                   Хочеш здати? Натискай!
                 </button>
               )}
@@ -178,12 +170,7 @@ console.log("block", block)
           projectId={myProject._id}
         />
       )}
-      {userRole === 3 && (
-        <ProjectList
-          marathonId={marathon._id}
-          blockId={block._id}
-        />
-      )}
+      {userRole === 3 && <ProjectList marathonId={marathon._id} blockId={block._id} />}
     </>
   );
 }
